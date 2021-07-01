4fb535876eaee20ac302a877070ddaf5445776ed
Shaikh Irshad <shaikhij2000@gmail.com>
2021-07-01 10:45:46 +0530

    procfile changes

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const app = express();
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
 
 // Configure template Engine and Main File
 app.engine(
-  "hbs",
+  'hbs',
   exphbs({
-    defaultLayout: "main",
-    extname: ".hbs",
+    defaultLayout: 'main',
+    extname: '.hbs',
     helpers: {
       todaysDate: (date) => new Date(date),
     },
-  })
-);
+  }),
+)
 // Seting template Engine
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs')
 
 // routes
-app.get("/", (req, res) => {
-  res.render("home", {
-    msg: "This is home page",
-  });
-});
-app.get("/room-listing", (req, res) => {
-  const counter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  res.render("room-listing", { counter: counter });
-});
-app.get("/user-registration", (req, res) => {
-  res.render("user-registration", {
-    peoples: [{ name: "jhon smith" }, { name: "jhonny Bravo" }],
-  });
-});
+app.get('/', (req, res) => {
+  res.render('home', {
+    msg: 'This is home page',
+  })
+})
+app.get('/room-listing', (req, res) => {
+  const counter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  res.render('room-listing', { counter: counter })
+})
+app.get('/user-registration', (req, res) => {
+  res.render('user-registration', {
+    peoples: [{ name: 'jhon smith' }, { name: 'jhonny Bravo' }],
+  })
+})
 
-app.listen(3000, () => {
-  console.log("The web server has started on port 3000");
-});
+const port = process.env.PORT || 3000
+
+app.listen(port, function () {
+  console.log('The web server has started on port 3000')
+})
